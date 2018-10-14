8aecd2fe896b233ff19a36786c1c385a53308a04
Lucas McCarthy <lucaspmccarthy@gmail.com>
2018-10-14 13:06:23 -0400

    Second Commit!

---
+++ src/main/java/com/lucas/personal/SpringDemo/topic/TopicRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.personal.SpringDemo.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+	
+	
+}
